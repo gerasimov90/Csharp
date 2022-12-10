d85d488a034dbe9212fde8095812281890ded03d
GerasimovVictor <gerasimov.viktor90@gmail.com>
2022-12-10 15:16:08 +0700

    семинар2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Console.Write("Введите любое трехзначное число:");
+//int number = Convert.ToInt32(Console.ReadLine());
+
+//int firstnumb = number / 100;
+
+//int secondnumb = (number - firstnumb*100)/10;
+
+//Console.Write("Вторая цифра введенного числа: ");
+//Console.WriteLine(secondnumb);
+
+
+
+
+
+Console.Write("Введите любое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else if (number < 1000 && number >= 100)
+{
+    int thirdnumb = number % 100;
+
+}
+Console.Write("Третья цифра введенного числа: ");
+Console.WriteLine(thirdnumb);
