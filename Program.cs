6d92e0c9792ad183a04b5fab5f0d6422efe29635
GerasimovVictor <gerasimov.viktor90@gmail.com>
2022-12-10 15:55:24 +0700

    семинар2

--- Program.cs
+++ Program.cs
@@ -12,17 +12,45 @@
 
 
 
-Console.Write("Введите любое число:");
+//Console.Write("Введите любое число:");
+//int number = Convert.ToInt32(Console.ReadLine());
+//
+//if (number < 100)
+//{
+//   Console.WriteLine("Третьей цифры нет");
+//}
+//else if (number < 1000 && number >= 100)
+//{
+//    int thirdnumb = number % 10;
+//
+//
+//Console.Write("Третья цифра введенного числа: ");
+//Console.WriteLine(thirdnumb);
+//}
+//
+//else if (number < 10000 && number >= 1000)
+//{
+//    int thirdnumb = (number % 100)/10;
+//
+//
+//Console.Write("Третья цифра введенного числа: ");
+//Console.WriteLine(thirdnumb);
+//}
+
+
+Console.Write("Введите число, обозначающее день недели :");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number < 100)
+if (number < 6)
 {
-    Console.WriteLine("Третьей цифры нет");
+    Console.WriteLine("Нет, этот день не выходной");
 }
-else if (number < 1000 && number >= 100)
+else if (number <= 7 && number > 5)
 {
-    int thirdnumb = number % 100;
+    Console.WriteLine("Да, этот день выходной");
+}
 
+else if (number > 7)
+{
+Console.WriteLine("В неделе всего 7 дней)))");
 }
-Console.Write("Третья цифра введенного числа: ");
-Console.WriteLine(thirdnumb);
